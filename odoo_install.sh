d42514f97bdc175f559bb674f13ffd0f26672826
lymanchen <lyman.chen@astasys.com>
2025-09-19 15:44:42 +0800

    Change service to systemd

--- odoo_install.sh
+++ odoo_install.sh
@@ -231,85 +231,36 @@ sudo chmod 755 $OE_HOME_EXT/start.sh
 #--------------------------------------------------
 # Adding ODOO as a deamon (initscript)
 #--------------------------------------------------
+echo -e "* Create systemd service file"
+cat <<EOF | sudo tee /etc/systemd/system/${OE_CONFIG}.service > /dev/null
+[Unit]
+Description=Odoo
+Requires=postgresql.service
+After=network.target postgresql.service
 
-echo -e "* Create init file"
-cat <<EOF > ~/$OE_CONFIG
-#!/bin/sh
-### BEGIN INIT INFO
-# Provides: $OE_CONFIG
-# Required-Start: \$remote_fs \$syslog
-# Required-Stop: \$remote_fs \$syslog
-# Should-Start: \$network
-# Should-Stop: \$network
-# Default-Start: 2 3 4 5
-# Default-Stop: 0 1 6
-# Short-Description: Enterprise Business Applications
-# Description: ODOO Business Applications
-### END INIT INFO
-PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+[Service]
+Type=simple
+SyslogIdentifier=${OE_CONFIG}
+PermissionsStartOnly=true
+User=$OE_USER
+Group=$OE_USER
+ExecStart=/$OE_USER/venv/bin/python ${OE_HOME_EXT}/odoo-bin -c /etc/${OE_CONFIG}.conf
+WorkingDirectory=${OE_HOME_EXT}
+StandardOutput=journal+console
+Restart=always
 
-# Virtual environment path
-DAEMON="/$OE_USER/venv/bin/python"
-DAEMON_ARGS="${OE_HOME_EXT}/odoo-bin"
-NAME=$OE_CONFIG
-DESC=$OE_CONFIG
-# Specify the user name (Default: odoo).
-USER=$OE_USER
-# Specify an alternate config file (Default: /etc/openerp-server.conf).
-CONFIGFILE="/etc/${OE_CONFIG}.conf"
-# pidfile
-PIDFILE=/var/run/\${NAME}.pid
-
-# Additional options that are passed to the Daemon.
-DAEMON_OPTS="\$DAEMON_ARGS -c \$CONFIGFILE"
-[ -x \$DAEMON ] || exit 0
-[ -f \$CONFIGFILE ] || exit 0
-checkpid() {
-[ -f \$PIDFILE ] || return 1
-pid=\`cat \$PIDFILE\`
-[ -d /proc/\$pid ] && return 0
-return 1
-}
-case "\${1}" in
-start)
-echo -n "Starting \${DESC}: "
-start-stop-daemon --start --quiet --pidfile \$PIDFILE \
---chuid \$USER --background --make-pidfile \
---exec \$DAEMON -- \$DAEMON_OPTS
-echo "\${NAME}."
-;;
-stop)
-echo -n "Stopping \${DESC}: "
-start-stop-daemon --stop --quiet --pidfile \$PIDFILE \
---oknodo
-echo "\${NAME}."
-;;
-restart|force-reload)
-echo -n "Restarting \${DESC}: "
-start-stop-daemon --stop --quiet --pidfile \$PIDFILE \
---oknodo
-sleep 1
-start-stop-daemon --start --quiet --pidfile \$PIDFILE \
---chuid \$USER --background --make-pidfile \
---exec \$DAEMON -- \$DAEMON_OPTS
-echo "\${NAME}."
-;;
-*)
-N=/etc/init.d/\$NAME
-echo "Usage: \$NAME {start|stop|restart|force-reload}" >&2
-exit 1
-;;
-esac
-exit 0
+[Install]
+WantedBy=multi-user.target
 EOF
 
-echo -e "* Security Init File"
-sudo mv ~/$OE_CONFIG /etc/init.d/$OE_CONFIG
-sudo chmod 755 /etc/init.d/$OE_CONFIG
-sudo chown root: /etc/init.d/$OE_CONFIG
+echo -e "* Reload systemd daemon"
+sudo systemctl daemon-reload
 
-echo -e "* Start ODOO on Startup"
-sudo update-rc.d $OE_CONFIG defaults
+echo -e "* Enable Odoo service to start on boot"
+sudo systemctl enable ${OE_CONFIG}.service
+
+echo -e "* Start Odoo service"
+sudo systemctl start ${OE_CONFIG}.service
 
 #--------------------------------------------------
 # Install Nginx if needed
